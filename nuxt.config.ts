800fb766562b91c4d83b9f7de6e9de19580fdbac
Sethy Rung <rungsethyhk@gmail.com>
2025-09-04 20:10:31 +0700

    chore: update Nuxt configuration and dependencies, replacing @nuxtjs/google-fonts with @nuxt/fonts and removing unused settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-
   css: ["~/assets/css/main.css"],
-
   modules: [
     "@nuxt/ui",
     "@nuxt/eslint",
@@ -10,26 +7,16 @@ export default defineNuxtConfig({
     "pinia-plugin-persistedstate/nuxt",
     "@vueuse/nuxt",
     "dayjs-nuxt",
-    "@nuxtjs/google-fonts",
+    "@nuxt/fonts",
   ],
-
   runtimeConfig: {
     apiBaseUrl: "",
     public: {
       appVersion: "",
     },
   },
-
   colorMode: {
     preference: "light",
   },
-
-  googleFonts: {
-    download: true,
-    families: {
-      "IBM Plex Sans": [100, 200, 300, 400, 500, 600, 700],
-    },
-  },
-
   compatibilityDate: "2025-03-04",
 });
